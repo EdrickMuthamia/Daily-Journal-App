dfc1ac66c5443eadaf53e1800a5699edb78a95dd
EdrickMuthamia <edrickmuthamia05@gmail.com>
2025-07-30 20:08:32 +0300

    add a function

--- src/page/NotFound.jsx
+++ src/page/NotFound.jsx
@@ -1,10 +1,8 @@
-import { Link } from 'react-router-dom';
-
 function NotFound() {
   return (
     <div className="not-found">
       <h1>404 - Page Not Found</h1>
-      <Link to="/" className="btn">Go Home</Link>
+      <button onClick={() => window.location.href = '/'} className="btn">Go Home</button>
     </div>
   );
 }
