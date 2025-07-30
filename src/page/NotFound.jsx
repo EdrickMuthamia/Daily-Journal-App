7607da15dd8d0962c6ae4283c9eb9b6fe6948126
EdrickMuthamia <edrickmuthamia05@gmail.com>
2025-07-31 02:45:06 +0300

    update

--- src/page/NotFound.jsx
+++ src/page/NotFound.jsx
@@ -2,7 +2,7 @@ function NotFound() {
   return (
     <div className="not-found">
       <h1>404 - Page Not Found</h1>
-      <button onClick={() => window.location.href = '/'} className="btn">Go Home</button>
+      <button onClick={() => window.location = '/'} className="btn">Go Home</button>
     </div>
   );
 }
