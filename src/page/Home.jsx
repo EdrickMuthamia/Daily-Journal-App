d8e5fb944ddeb1a8fcc0fd0c917436c6a8b3f16a
EdrickMuthamia <edrickmuthamia05@gmail.com>
2025-07-30 20:08:16 +0300

    add change

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,12 +1,10 @@
-import { Link } from 'react-router-dom';
-
 function Home() {
   return (
     <div className="home">
       <h1>Welcome to Daily Journal</h1>
       <p>Write and manage your daily thoughts</p>
-      <Link to="/add" className="btn">Write New Entry</Link>
-      <Link to="/entries" className="btn">View Entries</Link>
+      <button onClick={() => window.location.href = '/add'} className="btn">Write New Entry</button>
+      <button onClick={() => window.location.href = '/entries'} className="btn">View Entries</button>
     </div>
   );
 }
